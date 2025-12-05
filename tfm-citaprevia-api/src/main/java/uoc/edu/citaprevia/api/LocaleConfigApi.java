385eaa9e943f00e8ba96d83ae97735e783dbc9ab
gsimonet <gsimonet@uoc.edu>
2025-12-05 22:58:52 +0100

    afegir localeConfig al API

---
+++ tfm-citaprevia-api/src/main/java/uoc/edu/citaprevia/api/LocaleConfigApi.java
@@ -0,0 +1,39 @@
+package uoc.edu.citaprevia.api;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+/**
+ * Classe típica per interceptar el lang que prové de les crides rest del front-end
+ */
+public class LocaleConfigApi implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("ca")); 
+        return slr;
+    }
+
+    // Interceptor
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang"); // El client del front-end envia paràmetre lang
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+}
